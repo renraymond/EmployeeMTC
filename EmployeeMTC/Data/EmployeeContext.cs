cfb28d246c239af8732748b4b6716fb2bbcc7dc0
Raymond Ren <rren@dapasoft.com>
2021-05-08 10:59:04 -0400

    Add project files.

---
+++ EmployeeMTC/Data/EmployeeContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeMTC.Models;
+
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext (DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmployeeMTC.Models.Employee> Employee { get; set; }
+    }
